e66b9451cdb8f1ce456bce62f00afc9c489ecdf0
Piyush Shakya <piyushshakya112@gmail.com>
2025-12-14 23:28:14 +0530

    adding get method

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.use(cors({
 app.use('/api/v1/user',userRoutes);
 app.use('/api/v1/place',placeRoutes);
 
+app.get('/',(req , res )=>{
+    res.send("Backend is running")
+})
+
 connectDb()
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
